d64b0b689c36c936794c9f77fa202d942773e07f
nekkox <108139374+nekkox@users.noreply.github.com>
2024-01-28 13:02:25 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import ListItem from "./components/ListItem.vue";
+</script>
+
+<template>
+  <header>
+    <h1><span class="icon" aria-hidden="true">✅</span> To do</h1>
+    <p>Building App with Vue.js 3</p>
+  </header>
+
+  <ul>
+    <ListItem :is-checked="false">This is the slotted content</ListItem>
+  </ul>
+</template>
+
+<style scoped>
+header {
+  border-bottom: #333 1px solid;
+  background-color: #fff;
+}
+header::after {
+  content: "";
+  display: block;
+  height: 1px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+}
+h1 {
+  font-size: 2rem;
+}
+h1 .icon {
+  font-size: 1rem;
+  vertical-align: middle;
+}
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  isChecked: {
+    type: Boolean,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <label :class="{ checked: isChecked }">
+    <input type="checkbox" :checked="isChecked" />
+    <slot></slot>
+  </label>
+</template>
+
+<style scoped>
+label {
+  cursor: pointer;
+}
+.checked {
+  text-decoration: line-through;
+}
+</style>
